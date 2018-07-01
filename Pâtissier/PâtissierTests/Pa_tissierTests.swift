9895c01ebbbfc34f294a20320c441ecb13be2564
JerryCheWei <jerrychang585@gmail.com>
2018-07-01 09:44:31 +0800

    Add Patissier Xcode project

---
+++ Pâtissier/PâtissierTests/Pa_tissierTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pa_tissierTests.swift
+//  PâtissierTests
+//
+//  Created by chang-che-wei on 2018/6/30.
+//  Copyright © 2018年 chang-che-wei. All rights reserved.
+//
+
+import XCTest
+@testable import Pa_tissier
+
+class Pa_tissierTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
